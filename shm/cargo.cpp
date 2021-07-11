4b803ef62d9b15d818b62d0c4ded80833eb0c533
Wojtek Płóciennik <wojtek.plociennik@gmail.com>
2021-07-11 13:02:29 +0200

    Adding 3 extra classes

--- shm/cargo.cpp
+++ shm/cargo.cpp
@@ -1,25 +1,6 @@
 #include "cargo.hpp"
 
-Cargo& Cargo::operator+=(size_t amount) {
-    amount_ += amount;
-    return *this;
-}
+#include <iostream>
 
-Cargo& Cargo::operator-=(size_t amount) {
-    amount_ -= amount;
-    return *this;
-}
-
-bool Cargo::operator == (const Cargo& cargo) const {
-    return cargo.getName() == name_;
-} 
-
-std::string Cargo::getName() const {
-    return name_;
-}
-size_t Cargo::getAmount() const {
-    return amount_;
-}
-size_t Cargo::getBasePrice() const {
-    return basePrice_;
-}
+Cargo::Cargo(const std::string& name, size_t amount, size_t basePrice)
+    : name_(name), amount_(amount), basePrice_(basePrice){};
--- shm/main.cpp
+++ shm/main.cpp
@@ -1,17 +1,64 @@
 #include <iostream>
+#include <memory>
+
 #include "cargo.hpp"
-#include "ship.hpp"
 #include "player.hpp"
+#include "ship.hpp"
+#include "fruit.hpp"
+#include "alcohol.hpp"
+#include "item.hpp"
+
+void print(const Fruit& sth) {
+    std::cout << "nazwa: " << sth.getName() << ", amount: " << sth.getAmount() << ", base price: " << sth.getBasePrice() << ", dacayDays: " << sth.getExpirationDate() 
+              << " cena: " << sth.getPrice() << ", dni od kupienia: "<< sth.getPurchaseData() << '\n';
+}
 
 int main() {
+    
+    Fruit apple("pomarancza", 50, 30, 14);
+    print(apple);
+    apple += 10;
+    print(apple);
 
-    Cargo testCargo("test cargo", 12, 13);
-    Ship testShip(5000, 50, 30, "Lover", 10);
-    
-    Player Wojtek(testShip, 10, 20000);
-    
-    std::cout << Wojtek.getAvailableSpace();
-    
-   
+    Fruit sth("pomarancza", 30, 10, 2);
+
+    if(apple == sth) {
+        apple +=sth.getAmount();
+    }
+
+    --apple;
+    apple--;
+
+    print(apple);
+
+    // Alcohol a1("kola", 2, 2, 2);
+    Fruit f2("jabłko", 10, 20, 10);
+
+    Item i1("osc", 2, 2, Rarity::common);
+    Item i2("osc", 2, 3, Rarity::legendary);
+
+    std::cout << "Ilość osci: " << i1.getAmount() << '\n';
+    i1+= 20;
+    std::cout << "Ilość osci: " << i1.getAmount() << '\n';
+    f2+= 10;
+    i1 -= 10;
+    std::cout << "Ilość osci: " << i1.getAmount() << '\n';
+    if(i1 == i2) {
+        i1 += i2.getAmount();
+    }
+    std::cout << "Ilość osci: " << i1.getAmount() << '\n';
+    std::cout << "cena osci 1: " << i1.getPrice() << '\n';
+    std::cout << "cena osci 2: " << i2.getPrice() << '\n';
+
+    Alcohol a1("whisky", 10, 10 ,50);
+    a1 += 20;
+
+    std::cout << "Whisky ile go mam:  " << a1.getAmount()<< '\n';
+
+    std::cout << "Cena aktualna: " << a1.getPrice() << '\n';
+
+    print(apple);
+
     return 0;
+
 }
