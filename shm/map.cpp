9affaaa5cd253bccca475c326a52d864d629a291
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2021-07-13 00:05:39 +0200

    trying to move random generator to the header.. uncompleteted - working commit

--- shm/map.cpp
+++ shm/map.cpp
@@ -3,14 +3,15 @@
 #include "island.hpp"
 
 Map::Map() {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> widthDistribution(0, maxWidth_);
-    std::uniform_int_distribution<> heightDistribution(0, maxHeight_);
+    // std::random_device rd;
+    // std::mt19937 gen(rd());
+    // std::uniform_int_distribution<> widthDistribution(0, maxWidth_);
+    // std::uniform_int_distribution<> heightDistribution(0, maxHeight_);
 
+    randomGenerator_(randomDevice_());
     islands_.reserve(10);
     for (int i = 0; i < islandsQuantity_; ++i) {
-        Island tempIsland(widthDistribution(gen), heightDistribution(gen));
+        Island tempIsland(widthDistribution_(randomGenerator_), heightDistribution(gen));
         islands_.push_back(tempIsland);
     }
 }
